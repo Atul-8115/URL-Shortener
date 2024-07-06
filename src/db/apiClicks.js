4611951939120a1139dcbe5c6facfb5228269c89
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-07-06 11:34:11 +0530

    chore: Update click tracking logic and add redirect functionality

--- src/db/apiClicks.js
+++ src/db/apiClicks.js
@@ -1,3 +1,4 @@
+import { UAParser } from "ua-parser-js";
 import supabase from "./supabase";
 
 
@@ -8,6 +9,45 @@ export async function getClicksForUrls(urlIds) {
     if(error) {
         console.log("ERROR MESSAGE: ",error.message)
         throw new Error("Unable to load Clicks");
+    }
+
+    return data;
+}
+
+const parser = new UAParser();
+
+export const storeClicks = async ({ id, originalUrl}) => {
+    try {
+
+        console.log("I'm here in storeClicks")
+        const res = parser.getResult();
+        const device = res.type || "desktop"
+
+        const response = await fetch("https://ipapi.co/json")
+        const {city, country_name: country} = await response.json();
+
+        await supabase.from("clicks").insert({
+            url_id: id,
+            city: city,
+            country: country,
+            device: device,
+        });
+
+        window.location.href = originalUrl
+    } catch (error) {
+        console.log("ERROR MESSAGE: ",error.message)
+        throw new Error("Unable to load clicks")
+    }
+}
+
+export async function getClickForUrl(url_id) {
+    const {data, error} = await supabase
+                    .from("clicks")
+                    .select("*")
+                    .eq("url_id",url_id)
+    if(error) {
+        console.log("ERROR MESSAGE: ",error.message)
+        throw new Error("Unable to load Stats");
     }
 
     return data;
--- src/db/apiUrl.js
+++ src/db/apiUrl.js
@@ -55,11 +55,13 @@ export async function createUrl({title, longUrl, customUrl, user_id}, qrcode) {
 }
 
 export async function getLongUrl(id) {
-    const {data, error} = await supabase
+    let {data, error} = await supabase
                     .from("urls")
                     .select("id, original_url")
                     .or(`short_url.eq.${id},custom_url.eq.${id}`)
                     .signle();
+    
+    console.log("Printing in getLongUrl -> ",data)
     if(error) {
         console.log("ERROR MESSAGE: ",error.message)
         throw new Error("Unable to load the URL")
@@ -68,26 +70,19 @@ export async function getLongUrl(id) {
     return data
 }
 
-const parser = new UAParser();
-
-export const storeClicks = async ({ id, originalUrl}) => {
-    try {
-        const res = parser.getResult();
-        const device = res.type || "desktop"
-
-        const response = await fetch("https://ipapi.co/json")
-        const {city, country_name: country} = await response.json();
-
-        await supabase.from("clicks").insert({
-            url_id: id,
-            city: city,
-            country: country,
-            device: device,
-        });
-
-        window.location.href = originalUrl
-    } catch (error) {
+export async function getUrl({id, user_id}) {
+    const {data, error} = await supabase
+                    .from("urls")
+                    .select("*")
+                    .eq("id",id)
+                    .eq("user_id", user_id)
+                    .single();
+    if(error) {
         console.log("ERROR MESSAGE: ",error.message)
-        throw new Error("Unable to load clicks")
+        throw new Error("Short Url not found");
     }
+
+    return data;
 }
+
+
